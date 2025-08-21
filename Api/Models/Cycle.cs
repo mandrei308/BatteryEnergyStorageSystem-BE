268ad226c11e005336c98cf06f0b93e891ca1c2d
Mitrea Andrei <andrei.mitrea308@gmail.com>
2025-08-21 23:11:09 +0300

    Refactor + start for one cycle part

---
+++ Api/Models/Cycle.cs
@@ -0,0 +1,3 @@
+namespace Api.Models;
+
+public record Cycle(DateTime[] Sell, DateTime[] Buy);
--- Tests/EnergyAlgoTests.cs
+++ Tests/EnergyAlgoTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+﻿using System.Text;
 
 using Api;
 using Api.Models;
@@ -9,82 +9,90 @@ namespace Tests;
 
 public class EnergyAlgoTests
 {
-
-    [Fact]
-    public void TestReadCsv()
+    [Theory, MemberData(nameof(OptimalIntervalCases))]
+    public void TestCalculateOptimalInterval(string file, int initialEnergy, int cap, int maxPower, Interval expected)
     {
-        var result = ReadCsv("data/lines.csv");
-        Assert.NotNull(result);
-        Assert.True(result.Count > 0);
+        // Arrange
+        EntryData entryData = new(
+            InitialEnergy: initialEnergy,
+            Capacity: cap,
+            MaximumPower: maxPower,
+            Lines: ReadExcel(file));
+
+        // Act
+        var actual = EnergyAlgo.CalculateOptimalInterval(entryData);
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 
-    [Fact]
-    public void TestCalculateOptimalInterval()
+    public static TheoryData<string, int, int, int, Interval> OptimalIntervalCases = new()
     {
-        EntryData entryData = new
-        (
-            InitialEnergy: 12,
-            Capacity: 24,
-            MaximumPower: 6,
-            Lines: ReadExcel("data/10_a.xls")
-        );
+        { "data/18_a.xls", 12, 24, 6, new(Buy: new DateTime(2024, 9, 18, 14, 45, 0), Sell: new DateTime(2024, 9, 18, 9, 0, 0)) },
+        { "data/10_a.xls", 12, 24, 6, new(Buy: new DateTime(2024, 9, 10, 00, 45, 0), Sell: new DateTime(2024, 9, 10, 21, 0, 0)) }
+    };
 
-        var sellTimestamp = new DateTime(2024, 9, 18, 9, 0, 0);
-        var buyTimestamp = new DateTime(2024, 9, 18, 14, 45, 0);
+    [Theory, MemberData(nameof(OneCycleCases))]
+    public void TestCalculateOneCycle(string file, int initialEnergy, int cap, int maxPower, int intervalCount, Cycle expected)
+    {
+        // Arrange
+        EntryData entryData = new(
+            InitialEnergy: initialEnergy,
+            Capacity: cap,
+            MaximumPower: maxPower,
+            Lines: ReadExcel(file));
 
-        var result = EnergyAlgo.CalculateOptimalInterval(entryData);
-        Assert.NotNull(result);
-        Assert.True(result.Sell == sellTimestamp);
-        Assert.True(result.Buy == buyTimestamp);
+        // Act
+        var actual = EnergyAlgo.CalculateOneCycle(entryData, intervalCount);
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 
-    public static List<Line> ReadCsv(string filePath)
+    public static TheoryData<string, int, int, int, int, Cycle> OneCycleCases = new()
     {
-        var entries = new List<Line>();
-        var culture = new CultureInfo("en-US");
-        using (var reader = new StreamReader(filePath))
-        {
-            string header = reader.ReadLine()!;
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine()!;
-                var fields = line.Split(',');
-
-                for (int i = 0; i < fields.Length; i++)
-                {
-                    fields[i] = fields[i].Trim('\"');
-                }
-
-                var entry = new Line
-                (
-                    Timestamp: DateTime.Parse(fields[0], culture),
-                    Price: double.Parse(fields[1], NumberStyles.Any, culture)
-                );
-                entries.Add(entry);
-            }
-        }
-        return entries;
-    }
+        { "data/18_b.xls", 2, 24, 6, 4, new(Buy: [
+            new(2024, 9, 18, 15, 15, 0),
+            new(2024, 9, 18, 15, 30, 0),
+            new(2024, 9, 18, 15, 45, 0),
+            new(2024, 9, 18, 16, 00, 0)
+        ], Sell: [
+            new(2024, 9, 18, 20, 0, 0),
+            new(2024, 9, 18, 20, 15, 0),
+            new(2024, 9, 18, 20, 30, 0),
+            new(2024, 9, 18, 20, 45, 0)
+        ]) },
+        { "data/10_b.xls", 2, 24, 6, 4, new(Buy: [
+            new(2024, 9, 10, 4, 15, 0),
+            new(2024, 9, 10, 4, 30, 0),
+            new(2024, 9, 10, 4, 45, 0),
+            new(2024, 9, 10, 5, 0, 0)
+        ], Sell: [
+            new(2024, 9, 10, 20, 30, 0),
+            new(2024, 9, 10, 20, 45, 0),
+            new(2024, 9, 10, 21, 0, 0),
+            new(2024, 9, 10, 21, 15, 0)
+        ]) }
+    };
 
     public static List<Line> ReadExcel(string filePath)
     {
-        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
         var entries = new List<Line>();
-        using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
-        using (var reader = ExcelReaderFactory.CreateReader(stream))
+        using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+        using var reader = ExcelReaderFactory.CreateReader(stream);
+        var result = reader.AsDataSet();
+        var table = result.Tables[0];
+        for (int i = 1; i < table.Rows.Count; i++) // skip header
         {
-            var result = reader.AsDataSet();
-            var table = result.Tables[0];
-            for (int i = 1; i < table.Rows.Count; i++) // skip header
-            {
-                var row = table.Rows[i];
-                var entry = new Line(
-                    Timestamp: DateTime.Parse(row[0].ToString()!),
-                    Price: double.Parse(row[1].ToString()!)
-                );
-                entries.Add(entry);
-            }
+            var row = table.Rows[i];
+            var entry = new Line(
+                Timestamp: DateTime.Parse(row[0].ToString()!),
+                Price: double.Parse(row[1].ToString()!)
+            );
+            entries.Add(entry);
         }
+
         return entries;
     }
 
